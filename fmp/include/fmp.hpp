4eec52782fd38833f2d6bc91c6559af143ec46ca
zy_ih <zy_ih@outlook.com>
2023-09-18 12:22:02 +0800

    添加项目文件。

---
+++ fmp/include/fmp.hpp
@@ -0,0 +1,1 @@
+constexpr auto test = 1;
---
+++ fmp_test/fmp_test.cpp
@@ -0,0 +1,11 @@
+﻿// fmp_test.cpp: 定义应用程序的入口点。
+//
+
+#include <iostream>
+#include <fmp.hpp>
+
+int main()
+{
+    std::cout << test << std::endl;
+    return 0;
+}
