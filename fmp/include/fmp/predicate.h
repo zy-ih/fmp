587948a83015d7f07d84c41a698905b2cb82ab21
zy_ih <zy_ih@outlook.com>
2023-09-21 17:39:33 +0800

    Update predicate.h

--- fmp/include/fmp/predicate.h
+++ fmp/include/fmp/predicate.h
@@ -2,17 +2,17 @@
 namespace fmp
 {
 	template<typename, template<typename...>typename TC>
-	struct is_template_instance_type
+	struct is_template_instance
 	{
 		constexpr static bool value = false;
 	};
 
-	template<template<typename...>typename TC, typename... Args>
-	struct is_template_instance_type<TC<Args...>, TC>
+	template<template<typename...>typename TC, typename... Ts>
+	struct is_template_instance<TC<Ts...>, TC>
 	{
 		constexpr static bool value = true;
 	};
 
 	template<typename T, template<typename...>typename TC>
-	constexpr bool is_template_instance_v = is_template_instance_type<T, TC>::value;
+	constexpr bool is_template_instance_v = is_template_instance<T, TC>::value;
 }
