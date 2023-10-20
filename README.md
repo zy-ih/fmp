05409d4aa906e804f538452e8e38bec8285442c2
zy_ih <zy_ih@outlook.com>
2023-10-20 15:21:21 +0800

    添加简介

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fmp
+fmp is a functional-style C++ template metaprogramming library used for type transformation.
+
+It requires at least enabling the C++20 option as it makes use of concept syntax. The library itself doesn't depend on any other library or even the standard library.
--- fmp_test/fmp_test.cpp
+++ fmp_test/fmp_test.cpp
@@ -10,10 +10,16 @@ struct sizeof4
     constexpr static auto value = sizeof(T) == 4;
 };
 
+static_assert(
+    std::is_same_v<lazy<type_list<int, double>>
+    ::append<char>
+    ::filter<sizeof4>
+    ::concat<repeat_t<int, 3, std::tuple>>
+    ::to<std::tuple>
+    ::type,
+    std::tuple<int, int, int, int>>);
+
 int main()
 {
-    constexpr lazy<type_list<int, double>>::append<char>::filter<sizeof4>::concat<repeat_t<int, 3, std::tuple>>::to<std::tuple>::type point4d{0, 2, 3, 4};
-
-    constexpr auto r = std::get<2>(point4d);
 
 }
